95356b948eb6aa0258168abc7a88e85cc9687009
hal3e <hal3e.git@gmail.com>
2023-05-12 18:47:18 +0200

    feat: add tokio Mutex implementation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use futures::future::join_all;
 use std::cell::RefCell;
-use tokio::{process::Command, time::Duration};
+
+use futures::future::join_all;
+use tokio::{process::Command, sync::Mutex, time::Duration};
 
 struct CommandConfig {
     cmd: String,
     args: Vec<String>,
+    default: String,
     sec: u64,
 }
 
@@ -12,43 +14,45 @@ fn get_config() -> Vec<CommandConfig> {
     vec![CommandConfig {
         cmd: "date".into(),
         args: vec!["+%T".into()],
+        default: "N/A".to_string(),
         sec: 1,
     }]
 }
 
 struct Runner {
-    cmd: RefCell<Command>,
-    cell: RefCell<String>,
+    cmd: Mutex<Command>,
+    output: RefCell<String>,
     sec: u64,
 }
 
 impl Runner {
-    fn new(cmd: Command, sec: u64) -> Self {
-        Self {
-            cmd: RefCell::new(cmd),
-            cell: RefCell::new("".to_string()),
-            sec,
-        }
+    async fn get_cmd_output(&self) -> String {
+        let result = self.cmd.lock().await.output().await.unwrap();
+        String::from_utf8(result.stdout).unwrap().trim().to_string()
+    }
+
+    fn update_output(&self, output: String) {
+        *self.output.borrow_mut() = output;
     }
 
     async fn run(&self) {
         loop {
-            let result = self.cmd.borrow_mut().output().await.unwrap();
-            *self.cell.borrow_mut() = String::from_utf8(result.stdout).unwrap().trim().to_string();
+            let output = self.get_cmd_output().await;
+            self.update_output(output);
 
             tokio::time::sleep(Duration::from_secs(self.sec)).await;
         }
     }
 }
 
-async fn read_output(runners: &[Runner]) {
+async fn write_output(runners: &[Runner]) {
     loop {
         for r in runners {
-            print!("{}  ", r.cell.borrow())
+            print!("{}  ", r.output.borrow())
         }
-
         println!();
-        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        tokio::time::sleep(Duration::from_millis(500)).await;
     }
 }
 
@@ -56,14 +60,26 @@ async fn read_output(runners: &[Runner]) {
 async fn main() {
     let runners: Vec<Runner> = get_config()
         .into_iter()
-        .map(|config| {
-            let mut c = Command::new(config.cmd);
-            c.args(config.args);
-            Runner::new(c, config.sec)
-        })
+        .map(
+            |CommandConfig {
+                 cmd,
+                 args,
+                 default,
+                 sec,
+             }| {
+                let mut cmd = Command::new(cmd);
+                cmd.args(args);
+
+                Runner {
+                    cmd: Mutex::new(cmd),
+                    output: RefCell::new(default),
+                    sec,
+                }
+            },
+        )
         .collect();
 
     let futures = join_all(runners.iter().map(|r| r.run()));
 
-    tokio::join!(futures, read_output(&runners));
+    tokio::join!(futures, write_output(&runners));
 }
